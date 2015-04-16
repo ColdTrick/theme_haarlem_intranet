bf44ce9d6a3b3c4561ab5c85730228feee387f1a
Jerome Bakker <jeabakker@coldtrick.com>
2015-04-16 13:24:05 +0200

    wirepost layout

--- start.php
+++ start.php
@@ -25,6 +25,7 @@ function theme_haarlem_intranet_init() {
 	elgg_extend_view('css/elgg', 'css/theme_haarlem_intranet/responsive');
 	
 	elgg_register_plugin_hook_handler("register", "menu:theme-haarlem-intranet-site", "theme_haarlem_intranet_site_menu");
+	elgg_register_plugin_hook_handler("register", "menu:entity", "theme_haarlem_intranet_thewire_entity_menu");
 	elgg_register_plugin_hook_handler("prepare", "menu:page", "theme_haarlem_intranet_prepare_page_menu_static");
 	elgg_register_plugin_hook_handler("prepare", "menu:page", "theme_haarlem_intranet_prepare_menu_icons");
 	elgg_register_plugin_hook_handler("prepare", "menu:owner_block", "theme_haarlem_intranet_prepare_menu_icons");
