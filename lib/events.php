fff7160afc77729db016b571425e124041bfff5b
Jerome Bakker <jeabakker@coldtrick.com>
2015-06-03 14:38:39 +0200

    added: add user to site based on profile sync

--- lib/events.php
+++ lib/events.php
@@ -47,3 +47,31 @@ function theme_haarlem_intranet_site_leave_event($event, $type, $object) {
 	
 	elgg_set_plugin_user_setting('notify', '0', $user_guid, 'mentions');
 }
+
+/**
+ * Update a user based on information from profile sync
+ *
+ * @param string $event  the name of the event
+ * @param string $type   the type of the event
+ * @param mixed  $object supplied object
+ *
+ * @return void
+ */
+function theme_haarlem_intranet_profile_sync_update_user($event, $type, $object) {
+	
+	if (empty($object) || !is_array($object)) {
+		return;
+	}
+	
+	$user = elgg_extract('entity', $object);
+	if (empty($user) || !elgg_instanceof($user, 'user')) {
+		return;
+	}
+	
+	$site = elgg_get_site_entity();
+	
+	if (!check_entity_relationship($user->getGUID(), 'member_of_site', $site->getGUID())) {
+		// not a member, so add
+		$site->addUser($user->getGUID());
+	}
+}
